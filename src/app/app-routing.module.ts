115ee4be4110dd0e8ccec352783e4d56ef19bbe1
Alex <alexkariuki47@outlook.com>
2019-07-28 08:39:30 +0300

    Designed the home page and the tabs, also added the rest of the pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  { path: '', redirectTo: 'tabs', pathMatch: 'full'},
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'list', loadChildren: () => import('./list/list.module').then(m => m.ListPageModule) },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'search', loadChildren: './search/search.module#SearchPageModule' },
+  { path: 'notifications', loadChildren: './notifications/notifications.module#NotificationsPageModule' },
+  { path: 'mail', loadChildren: './mail/mail.module#MailPageModule' }
 ];
 
 @NgModule({
---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: './home/home.module#HomePageModule'
+      }, {
+        path: 'search',
+        loadChildren: './search/search.module#SearchPageModule'
+      },
+      {
+        path: 'notifications',
+        loadChildren: './notifications/notifications.module#NotificationsPageModule'
+      }, {
+        path: 'mail',
+        loadChildren: './mail/mail.module#MailPageModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
